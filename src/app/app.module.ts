8ad603bfd22bb9feb8c5770a76e1b18293e3a380
Sajan John <sjohn2>
2017-03-31 10:02:33 -0400

    ng4 first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ContentProjectionComponent } from './content-projection/content-projection.component';
 
+const appRoutes: Routes = [   
+    { path: 'content-projection', component: ContentProjectionComponent }
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContentProjectionComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(
+      appRoutes,{useHash:true}
+    )   
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
